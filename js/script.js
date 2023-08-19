720d9d7c6020c8827579553af38d052a72fc3d76
aureline12 <motouomaureline@gmail.com>
2023-08-19 20:38:47 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const showMenu = () => {
+    document.getElementById('navbarContent').classList.add('show-menu');
+}
+
+const hideMenu = () => {
+    document.getElementById('navbarContent').classList.remove('show-menu');
+}
